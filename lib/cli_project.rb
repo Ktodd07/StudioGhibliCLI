6bd8dc9455add48ca462e01714edba3f4fcb484b
Ktodd07 <72150767+Ktodd07@users.noreply.github.com>
2020-12-17 12:47:45 -0500

    Update cli_project.rb

--- lib/cli_project.rb
+++ lib/cli_project.rb
@@ -1,7 +1,10 @@
 require_relative "cli_project/version"
-
 require_relative "./cli_project/api"
-
 require_relative "./cli_project/films"
-
 require_relative "./cli_project/cli"
+
+require 'json'
+require 'pry'
+require 'rest-client'
+require 'net/http'
+require 'colorize'
