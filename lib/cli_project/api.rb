19ceda3facbfdf2578f782de5a65b1a364f9f58c
Ktodd07 <72150767+Ktodd07@users.noreply.github.com>
2020-12-17 14:50:41 -0500

    Update api.rb

--- lib/cli_project/api.rb
+++ lib/cli_project/api.rb
@@ -2,9 +2,9 @@ class API
 
    def self.get_data
         uri = URI("https://ghibliapi.herokuapp.com/films")
-        data = Net::HTTP.get(uri)
-        response = JSON.parse(data)
-        response.each do |film|
+        response = Net::HTTP.get(uri)
+        data = JSON.parse(response)
+        data.each do |film|
             Films.new(film["title"], film["description"], film["director"], film["release_date"])
         end
     end
